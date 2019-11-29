830e98cffb2d16c3393d33ca283dfa7a2d04418d
blpilla <blpilla@yahoo.com.br>
2019-11-29 14:47:18 -0300

    Envio de dump.sql.

--- etapa 2/etapa2.sql
+++ etapa 2/etapa2.sql
@@ -1,16 +1,16 @@
-SELECT 
+SELECT
 	d.dept_name,
 	CONCAT(e.first_name, ' ', e.last_name) as full_name,
-	ABS(DATEDIFF(de.from_date, de.to_date)) as worked_days 
+	ABS(DATEDIFF(de.from_date, de.to_date)) as worked_days
 
-FROM 
+FROM
 	employees.dept_emp as de
 
 INNER JOIN employees.employees as e
-	ON de.emp_no = e.emp_no 
+	ON de.emp_no = e.emp_no
 
 INNER JOIN employees.departments as d
 	 ON de.dept_no = d.dept_no
-	 
-ORDER BY 
-	ABS(DATEDIFF(de.from_date, de.to_date)) DESC;
+
+ORDER BY
+	worked_days DESC;
